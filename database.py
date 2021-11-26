2f4b6fbc32d59f7c10793ca488c229381749fe18
dchekel <dchekel@gmail.com>
2021-11-26 11:20:26 +0300

    connect to db from sqlalchemy, model description

--- database.py
+++ database.py
@@ -1,17 +1,13 @@
 import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy import MetaData, Table, String, INTEGER, Column, Text, DateTime, Boolean
-from datetime import datetime
-from sqlalchemy import select
-# from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # DATABASE_URI = DBE + dbUser + ':' + dbPassword + '@' + dbServer + '/'+ dbName
 # engine=create_engine('postgresql://<username>:<password>@localhost/<db_name>'
 DATABASE_URI = 'postgresql+psycopg2://postgres_admin:pos12345@localhost:5432/postgres_db'
-# engine = create_engine(DATABASE_URI, echo=True)
 
 try:
-    engine = create_engine(DATABASE_URI)
+    engine = create_engine(DATABASE_URI)  # , echo=True)
     connection = engine.connect()
 
 except sqlalchemy.exc.OperationalError:
@@ -20,12 +16,12 @@ except sqlalchemy.exc.OperationalError:
 else:
     print('OK')
     print('connection to the database is established')
-input("Press Enter to continue...")
+# input("Press Enter to continue...")
+Base = declarative_base()
+Session = sessionmaker()
 
-from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean
-from datetime import datetime
 
-metadata = MetaData()
+
 '''username varchar(100) not null,
 password varchar(60) not null,
 first_name varchar(100),
@@ -35,7 +31,7 @@ address varchar(100) not null,
 primary key(id),
 unique(username),
 unique(email)
-'''
+
 user = Table('user', metadata,
              Column('id', Integer(), primary_key=True),
              Column('username', String(100), nullable=False),
@@ -45,7 +41,7 @@ user = Table('user', metadata,
              Column('email', String(100), nullable=False),
              Column('address', String(100), nullable=False)
              )
-
+'''
 # , DateTime(), default=datetime.now, onupdate=datetime.now)
 '''
 ins = user.insert().values(
@@ -57,11 +53,11 @@ ins = user.insert().values(
     address='Grodno, Boldina 400, appt. 400'
     )
 r = connection.execute(ins)
-'''
+
 s = select([user])
 r = connection.execute(s)
 print(s)
 print(r.fetchall())
+'''
 
-# Base = declarative_base()
-# Session = sessionmaker()
+#
---
+++ models.py
@@ -0,0 +1,108 @@
+from database import Base
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import MetaData, Table, Column, ForeignKey
+from sqlalchemy import String, REAL, Integer, Text, DateTime, Boolean
+# from sqlalchemy_utils.types import ChoiceType
+
+
+# https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#many-to-many
+association_table_user_role = Table('user_role', Base.metadata,
+                                    Column('user_id', ForeignKey('user.id'), primary_key=True),  # pk??
+                                    Column('role_id', ForeignKey('role.id'), primary_key=True)   # pk??
+                                    )
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True),
+    username = Column(String, nullable=False, unique=True),
+    password = Column(String, nullable=False),
+    first_name = Column(String),
+    last_name = Column(String),
+    email = Column(String, nullable=False, unique=True),
+    address = Column(String, nullable=False)
+    # relationship
+    roles = relationship("Role",
+                         secondary=association_table_user_role,  # многие-ко-многим
+                         back_populates="users")
+    payments = relationship("Payment", back_populates="users")
+    orders = relationship("Order", back_populates="users")
+
+    def __repr__(self):
+        return f"<User: {self.username}, Name: {self.first_name} {self.last_name}"
+
+
+class Role(Base):
+    __tablename__ = 'role'
+
+    id = Column(Integer, primary_key=True),
+    name = Column(String, nullable=False, unique=True)
+    # relationship
+    users = relationship("User",
+                         secondary=association_table_user_role,  # многие-ко-многим
+                         back_populates="roles")
+
+    def __repr__(self):
+        return f"<Role: {self.name}>"
+
+
+class Payment(Base):
+    __tablename__ = 'payment'
+
+    id = Column(Integer, primary_key=True),
+    amount = Column(REAL, nullable=False),
+    paid_at = Column(DateTime),
+    order_id = Column(Integer, ForeignKey('order.id'))  # nullable=False
+    user_id = Column(Integer, ForeignKey('user.id'))  # nullable=False
+    # relationship
+    users = relationship("User", back_populates="payments")
+    orders = relationship("Order", back_populates="payments")
+
+    def __repr__(self):
+        return f"<Payment(User: {self.user_id}, Sum: {self.amount}, At: {self.paid_at})>"
+
+
+class Order(Base):
+    __tablename__ = 'order'
+
+    id = Column(Integer, primary_key=True),
+    user_id = Column(Integer, ForeignKey('user.id'))  # nullable=False
+    status_id = Column(Integer, ForeignKey('order_status.id'))  # nullable=False
+    created_at = Column(DateTime),
+    closed_at = Column(DateTime),
+    total_cost = Column(REAL, nullable=False),
+    total_quantity = Column(REAL, nullable=False),
+    # relationship
+    users = relationship("User", back_populates="orders")
+    order_statuses = relationship("OrderStatus", back_populates="orders")
+    payments = relationship("Payment", back_populates="orders")
+
+    def __repr__(self):
+        return f"<Order(User: {self.user_id}, Cost: {self.total_cost})"
+
+
+class OrderStatus(Base):
+    __tablename__ = 'order_status'
+
+    id = Column(Integer, primary_key=True),
+    name = Column(String, nullable=False, unique=True)
+    # relationship
+    orders = relationship("Order", back_populates="order_statuses")
+
+    def __repr__(self):
+        return f"<OrderStatus: {self.name}"
+
+
+'''
+backref -
+Указывает строковое имя свойства, которое должно быть помещено в класс связанного сопоставления, который будет 
+обрабатывать это отношение в другом направлении. Другое свойство будет создано автоматически при настройке сопоставлений
+. Также может быть передано как объект backref() для управления конфигурацией нового отношения.
+
+back_populates -
+Принимает строковое имя и имеет то же значение, что и backref, за исключением того, что дополняющее свойство 
+не создается автоматически, а должно быть настроено явно на другом сопоставлении. Дополняющее свойство также должно 
+указывать back_populates на это отношение для обеспечения правильного функционирования.
+'''
