c9e6081d11ebecbd6ffc26750ae535d1ffe88273
dchekel <dchekel@gmail.com>
2021-12-22 10:45:00 +0300

    schemas user & role

--- app/core/schemas/schemas.py
+++ app/core/schemas/schemas.py
@@ -1,5 +1,5 @@
 # test auth
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, EmailStr
 
 
@@ -19,7 +19,7 @@ class UserBase(BaseModel):
 # Properties to receive via API on creation
 class UserCreate(UserBase):
     email: EmailStr
-    password: str
+    password: str  # пароль нужен только при создании пользователя
 
 
 # Properties to receive via API on update
@@ -27,11 +27,24 @@ class UserUpdate(UserBase):
     ...
 
 
+class Role(BaseModel):
+    id: int
+    name: str
+
+    class Config:  # иначе -  value is not a valid dict (type=type_error.dict)
+        orm_mode = True
+
+
 class UserInDBBase(UserBase):
     id: Optional[int] = None
+    # relationship  this from app/core/models/models.py class User(Base):
+    # roles = relationship("Role",
+    #                      secondary=association_table_user_role,  # многие-ко-многим
+    #                      back_populates="users")
+    roles: List[Role] = []
 
-    class Config:
-        orm_mode = True
+    class Config:  # иначе -  value is not a valid dict (type=type_error.dict)
+        orm_mode = True  # SQLAlchemy не возвращает словарь, чего ожидает pydantic по умолчанию
 
 
 # Additional properties stored in DB but not returned by API
@@ -42,3 +55,4 @@ class UserInDB(UserInDBBase):
 # Additional properties to return via API
 class User(UserInDBBase):
     ...
+
